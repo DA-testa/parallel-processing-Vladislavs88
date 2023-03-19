e558ea7a528f539c865287e561bfe95cd4a981c0
221RDB070 <125796943+Vladislavs88@users.noreply.github.com>
2023-03-19 17:30:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 # python3
+#Vladislavs Sidorkins 221RDB070
 
 def parallel_processing(n, m, data):
+    flow=[0]*n
     output = []
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
+    for i in range(m):
+        j=0
+        while j<n and flow[j]>0:
+            j+=1
+        
+        if j==n:
+            wait=min(flow)
+            flow=[change - wait for change in flow]
+            j=flow.index(0)
+        flow[j]=data[i]
+        output.append([j,sum(flow[:j+1])])
 
     return output
 
@@ -13,15 +26,22 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
+    n,m = map(int,input().split())
+    
+    data =list(map(int,input().split()))
+
+
+
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    
 
     # TODO: create the function
     result = parallel_processing(n,m,data)
+
+    for i,j in result:
+        print(i,j)
     
     # TODO: print out the results, each pair in it's own line
 
